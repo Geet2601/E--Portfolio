1755bdfa25d6db785b29d05e6b6ba045d7398610
Geet2601 <gt2601001gt@gmail.com>
2023-12-14 23:13:48 +0530

    commit

---
+++ js/components/Hero.jsx
@@ -0,0 +1,81 @@
+import "../dist/styles.css";
+import Waving from "../images/waving.png";
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
+
+const skillsIcons = [
+  {
+    img: "https://skillicons.dev/icons?i=html,css",
+    id: 1,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=js,ts",
+    id: 2,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=react,next",
+    id: 3,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=tailwind,scss",
+    id: 4,
+  },
+];
+
+function Hero() {
+  return (
+    <>
+      <section id="home" className="hero">
+        <div className="container">
+          <div className="content">
+            <div className="hero-main">
+              <div className="hero-text">
+                <h1>Front-End React Developer</h1>
+                <img src={Waving} alt="waving_hand" />
+                <p>
+                  Hi, I'm Geet Tiwari. A passionate Front-end React
+                  Developer based in Jhansi, India. 🍃
+                </p>
+                <span>
+                  <a
+                    aria-label="linkedin"
+                    rel="noreferrer"
+                    target="_blank"
+                    // href="https://www.linkedin.com/in/stefan-topalovic-dev/"
+                  >
+                    <IconBrandLinkedin width={32} height={32} />
+                  </a>
+                  <a
+                    aria-label="github"
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/stefvndev"
+                  >
+                    <IconBrandGithub width={32} height={32} />
+                  </a>
+                </span>
+              </div>
+
+              <div className="hero-img"></div>
+            </div>
+
+            {/*  */}
+            <div className="skills">
+              <p>Tech Stack</p>
+              <div className="logos">
+                <ul>
+                  {skillsIcons.map((icon) => (
+                    <li key={icon.id}>
+                      <img src={icon.img} alt="skill-icon" />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
---
+++ js/images/App.js
@@ -0,0 +1,12 @@
+import "../src/dist/styles.css";
+import Home from "./Pages/Hom.jsx";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
